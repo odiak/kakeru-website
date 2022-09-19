b05abd61c3b9c6a2698181ce334c64c23dfc69ec
Kaido Iwamoto <kaido@odiak.net>
2022-09-20 03:03:17 +0900

    Setup Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
 
+import tailwind from '@astrojs/tailwind'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), tailwind()],
 })
