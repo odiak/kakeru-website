c3a3ea2c371dc02efe0726b6aad731fa8e9706d3
Kaido Iwamoto <kaido@odiak.net>
2022-09-20 03:00:27 +0900

    Setup i18n

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLanguage: 'en',
+  supportedLanguages: ['en', 'ja'],
+  i18next: {
+    debug: true,
+    initImmediate: false,
+    backend: {
+      loadPath: './src/locales/{{lng}}.json',
+    },
+  },
+  i18nextPlugins: {
+    fsBackend: 'i18next-fs-backend',
+  },
+}
