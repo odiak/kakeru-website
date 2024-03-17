4ebb37898d1a485a409b21e87326f6b04bdb85a2
Kaido Iwamoto <kaido@odiak.net>
2024-03-18 03:27:05 +0900

    Setup icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
-
 import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), tailwind()],
+  integrations: [astroI18next(), tailwind(), icon()],
 })
